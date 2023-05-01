0c11af4391fe71e3c2046ec2bbda3c525217aa41
Felipe-Blest <102250682+Felipe-Blest@users.noreply.github.com>
2023-05-01 18:39:37 -0400

    Avance Sprint, falta último

--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', function (e) {
 	e.preventDefault()
 
+
 	let seleccion = document.getElementById("trabajador")
 	seleccion.addEventListener("change", function (e) {
 		opcion = e.target.value;
@@ -17,7 +18,7 @@ window.addEventListener('DOMContentLoaded', function (e) {
 	})
 
 
-// FECHA TOPE HOY EN FECHAS
+	// FECHA TOPE HOY EN FECHAS
 	let date = new Date(),
 		year = date.getFullYear(),
 		month = date.getMonth(),
@@ -35,7 +36,7 @@ window.addEventListener('DOMContentLoaded', function (e) {
 	query2.setAttribute('max', `${fecha}`)
 	console.log(fecha);
 
-//SI NO TIENE CARGAS, DESHABILITACIÓN DE INPUT CANTIDAD DE CARGAS
+	//SI NO TIENE CARGAS, DESHABILITACIÓN DE INPUT CANTIDAD DE CARGAS
 	let cargas = document.getElementById('carga')
 	cargas.addEventListener("change", function (e) {
 		opcion = e.target.value
@@ -48,114 +49,145 @@ window.addEventListener('DOMContentLoaded', function (e) {
 		}
 	})
 
-//Al tocar botón, recuperar información del localStorage
+	//Al tocar botón, recuperar información del localStorage
 	let boton = document.getElementById('boton')
 	boton.addEventListener("click", function () {
-//recuperar información del nombre y mostrarlo
+		//recuperar información del nombre y mostrarlo
 		escrituraJSON = document.getElementById('nombreForm')
 		escrituraJSON.setAttribute('value', localStorage.getItem('nombre'))
-//recuperar información del apellido y mostrarlo
+		//recuperar información del apellido y mostrarlo
 		escrituraJSON = document.getElementById('apellidoForm')
 		escrituraJSON.setAttribute('value', localStorage.getItem('apellido'))
-//recuperar información del sueldo y mostrarlo
+		//recuperar información del sueldo y mostrarlo
 		escrituraJSON = document.getElementById('sueldoForm')
 		escrituraJSON.setAttribute('value', localStorage.getItem('sueldo'))
-	})	
-//recuperar información de la verificacion asignacion y mostrarlo
-		escrituraJSON = document.getElementById('cargaForm')
-		escrituraJSON.setAttribute('value', localStorage.getItem('carga'))
+	})
+
 })
 
 
 let guardarcambios = () => {
-//buscar y guardar nombre
+	//buscar y guardar nombre
 	let nombre = document.getElementById('nombre').value;
 	console.log("El nombre es ->", nombre)
 	localStorage.setItem('nombre', nombre)
 	let JSON1 = console.log("El nombre sacado del localStorage ->", localStorage.getItem('nombre'));
 
-// buscar y guardar apellido
+	// buscar y guardar apellido
 	let apellido = document.getElementById('apellido').value;
 	console.log("El nombre es ->", apellido)
 	localStorage.setItem('apellido', apellido)
 	let JSON2 = console.log("El apellido sacado del localStorage ->", localStorage.getItem('apellido'));
-	
-// buscar y guardar sueldo
+
+	// buscar y guardar sueldo
 	let sueldo = document.getElementById('sueldo').value;
 	console.log("El nombre es ->", sueldo)
 	localStorage.setItem('sueldo', sueldo)
 	let JSON3 = console.log("El sueldo sacado del localStorage ->", localStorage.getItem('sueldo'));
 
-// buscar y guardar verificacion cargas
+	// buscar y guardar sueldo semestre anterior
+	let sueldoAnt = document.getElementById('semestreSem').value;
+	localStorage.setItem('sueldoAnt', sueldoAnt)
+	let JSON4 = console.log("El sueldo del semestre anterior es ->", localStorage.getItem('sueldoAnt'));
+
+	// buscar y guardar verificacion cargas
 	let carga = document.getElementById('carga').value;
 	console.log("Corresponde asignacion ->", carga)
 	localStorage.setItem('carga', carga)
-	let JSON4 = console.log("Corresponde carga ->", localStorage.getItem('carga'));
+	let JSON5 = console.log("Corresponde carga ->", localStorage.getItem('carga'));
+
+	// buscar y guardar cantidad de cargas
+	let cantCargas = document.getElementById('cantidadCargas').value
+	localStorage.setItem('cantCargas', cantCargas)
+	let JSON6 = console.log("Cantidad de cargas ->", localStorage.getItem('cantCargas'));
+
+	// buscar y guardar la fecha de ingreso a la organización
+	let fechaIng = document.getElementById('ingreso').value
+	localStorage.setItem('fechaIng', fechaIng)
+	let JSON7 = console.log("Fecha de ingreso org ->", localStorage.getItem('fechaIng'))
+
+
+	const formatter = new Intl.NumberFormat('es-CL', {
+		style: 'currency',
+		currency: 'CLP',
+		minimumFractionDigits: 0,
+		localeMatcher: 'lookup',
+	});
+
+	// operación para saber si corresponde asignación y la cantidad
+	let resultado = {}
+	if (localStorage.getItem('cantCargas') == 0 || localStorage.getItem('carga') == false) {
+		resultado.boolean = false
+		resultado.mensaje = 0
+	}
+	else if (localStorage.getItem('sueldoAnt') <= 429899) {
+		resultado.boolean = true
+		resultado.mensaje = `Asignación de $16.828 por ${localStorage.getItem('cantCargas')} cargas, en total recibes ${formatter.format(16828 * localStorage.getItem('cantCargas'))}`
+	}
+	else if (localStorage.getItem('sueldoAnt') > 429899 && localStorage.getItem('sueldoAnt') <= 627913) {
+		resultado.boolean = true
+		resultado.mensaje = `Asignación de $10.327 por ${localStorage.getItem('cantCargas')} cargas, en total recibes ${formatter.format(10327 * localStorage.getItem('cantCargas'))}`
+	}
+	else if (localStorage.getItem('sueldoAnt') > 627913 && localStorage.getItem('sueldoAnt') <= 979330) {
+		resultado.boolean = true
+		resultado.mensaje = `Asignación de $3.264 por ${localStorage.getItem('cantCargas')} cargas, en total recibes ${formatter.format(3264 * localStorage.getItem('cantCargas'))}`
+	}
+	else if (localStorage.getItem('sueldoAnt') > 979330) {
+		resultado.boolean = false
+		resultado.mensaje = 0
+	}
+	console.log(resultado.boolean)
+	console.log(resultado.mensaje)
+
+	//Rellenar campo de si corresponde información
+	if (resultado.boolean == false) {
+		correspondeAsig = document.getElementById('corrAsig')
+		correspondeAsig.setAttribute('value', 'No corresponde asignación familiar')
+	} else {
+		correspondeAsig = document.getElementById('corrAsig')
+		correspondeAsig.setAttribute('value', 'Sí corresponde asignación familiar')
+	}
+
+	//Rellenar campo con el sueldo de la asignación
+	sueldoAsig = document.getElementById('MontoAsig')
+	sueldoAsig.setAttribute('value', resultado.mensaje)
+
+	//Operación para saber el sueldo final
+	let sueldofinal;
+	if (localStorage.getItem('cantCargas') == 0 || localStorage.getItem('carga') == false) {
+		sueldofinal = `Su sueldo final es ${formatter.format(localStorage.getItem('sueldo'))}`
+	}
+	else if (localStorage.getItem('sueldoAnt') <= 429899) {
+		sueldofinal = `Su sueldo final es ${formatter.format(localStorage.getItem('sueldo') + (16828 * localStorage.getItem('cantCargas')))}`
+	}
+	else if (localStorage.getItem('sueldoAnt') > 429899 && localStorage.getItem('sueldoAnt') <= 627913) {
+		sueldofinal = `Su sueldo final es ${formatter.format(localStorage.getItem('sueldo') + (10327 * localStorage.getItem('cantCargas')))}`
+	}
+	else if (localStorage.getItem('sueldoAnt') > 627913 && localStorage.getItem('sueldoAnt') <= 979330) {
+		sueldofinal = `Su sueldo final es ${formatter.format(parseInt(localStorage.getItem('sueldo')) + parseInt((3264 * localStorage.getItem('cantCargas'))))}`
+	}
+	else if (localStorage.getItem('sueldoAnt') > 979330) {
+		sueldofinal = `Su sueldo final es ${formatter.format(localStorage.getItem('sueldo'))}`
+	}
+
+	documentSueldoFinal = document.getElementById('sueldoFin')
+	documentSueldoFinal.setAttribute('value', sueldofinal)
+
+	//Operación para saber la permanencia del usuario en la empresa
+	let fechaIngresada = localStorage.getItem('fechaIng');
+	fechaIngresada = new Date(fechaIngresada);
+	let fechaHoy = new Date();
+	const yearSelect = fechaHoy.getFullYear() - fechaIngresada.getFullYear();
+	const monthSelect = fechaHoy.getMonth() - fechaIngresada.getMonth();
+	const daysSelect = (fechaHoy.getDate() - fechaIngresada.getDate());
+	const ress = `${localStorage.getItem('nombre')} lleva una permanencia de ${yearSelect} años, ${monthSelect} meses y ${daysSelect} días`;
+	console.log(ress)
+	console.log("Año seleccionado", fechaHoy.getFullYear())
+	console.log("Año actual", fechaIngresada.getFullYear())
+
+	//Escribir tiempo de permanencia de acuerdo a operación
+	let tiempoPerm = document.getElementById('tiempoPermanencia')
+	tiempoPerm.setAttribute('value', ress)
+
 }
 
-
-
-
-
-// FUNCION PARA CALCULO DE ASIGNACION Y SUELDO FINAL
-
-function Afiliado(nombre, apellido, sueldoAct, sueldoSem, carga, nCargas) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.sueldo = sueldo;
-    this.sueldoSem = sueldoSem;
-    this.carga = carga;
-    this.nCargas = nCargas;
-
-    this.beneficiario = function () {
-        return `El beneficiario es ${this.nombre} ${this.apellido}`
-    }
-
-    this.sueldoActual = function () {
-        return `El sueldo de ${this.nombre} ${this.apellido} es ${formatter.format(this.sueldo)}`
-    }
-
-    this.asignacion = function () {
-        if (!nCargas) {
-            return `No calificas para la asignacion familiar`
-        }
-        else if (sueldoSem <= 429899) {
-            return `Su asignacion familiar es de $16,828 por carga en total recibes ${formatter.format(16828 * this.nCargas)}`
-        }
-        else if (sueldoSem > 429899 && sueldoSem <= 627913) {
-            return `Su asignacion familiar es de $10,327 por carga en total recibes ${formatter.format(10327 * this.nCargas)}`
-        }
-        else if (sueldoSem > 627913 && sueldoSem <= 979330) {
-            return `Su asignacion familiar es de $3,264 por carga en total recibes ${formatter.format(3264 * this.nCargas)}`
-        }
-        else if (sueldoSem > 979330) {
-            return `No calificas para la asignacion familiar`
-        }
-    }
-
-    this.sueldoFinal = function () {
-        if (!nCargas) {
-            return `Su sueldo final es ${formatter.format(this.sueldo)}`
-        }
-        else if (sueldoSem <= 429899) {
-            return `Su sueldo final es ${formatter.format(this.sueldo + (16828 * this.nCargas))}`
-        }
-        else if (sueldoSem > 429899 && sueldoSem <= 627913) {
-            return `Su sueldo final es ${formatter.format(this.sueldo + (10327 * this.nCargas))}`
-        }
-        else if (sueldoSem > 627913 && sueldoSem <= 979330) {
-            return `Su sueldo final es ${formatter.format(this.sueldo + (3264 * this.nCargas))}`
-        }
-        else if (sueldoSem > 979330) {
-            return `Su sueldo final es ${formatter.format(this.sueldo)}`
-        }
-    }
-}
-
-console.log('====== AFILIADO 5 =======')
-let Afiliado6 = new Afiliado('Gabriel', 'Martinez', 100000, 100000, false, 0)
-console.log(Afiliado6)
-console.log(Afiliado6.beneficiario())
-console.log(Afiliado6.sueldoActual())
-console.log(Afiliado6.asignacion())
-console.log(Afiliado6.sueldoFinal())
